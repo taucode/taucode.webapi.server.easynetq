3a15270024c87b486bdc62a085a4189782310a10
Andrey Kovalenko <ak@taucode.com>
2020-03-24 23:22:45 +0200

    Inited.

---
+++ tests/TauCode.WebApi.Server.EasyNetQ.Tests/WebApiServerEasyNetQDummyFixture.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace TauCode.WebApi.Server.EasyNetQ.Tests
+{
+    [TestFixture]
+    public class WebApiServerEasyNetQDummyFixture
+    {
+        [Test]
+        public void WebApiServerEasyNetQDummyTest()
+        {
+            Assert.Pass("Added for CI.");
+        }
+    }
+}
